0a4628efc617048997f8c4a34bb317f741755991
Mazhar <65758011+MostafaMazhar@users.noreply.github.com>
2023-06-17 19:41:10 +0000

    added h1 & fragment tag -App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import "./App.css";
 
 export default function App() {
   return (
-    <form className="new-item-form">
-      <div className="form-row">
-        <label htmlFor="item">New Item</label>
-        <input type="text" id="item" />
-      </div>
-      <button className="btn">Add</button>
-    </form>
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">To Do List</h1>
+    </>
   );
 }
