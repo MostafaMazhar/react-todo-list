f2bef7c23ef0a6ef057a1ee56f456bd073f5b27d
Mazhar <65758011+MostafaMazhar@users.noreply.github.com>
2023-06-17 20:21:26 +0000

    Update App.jsx function handleSubmit

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import "./App.css";
 
 export default function App() {
   const [newItem, setNewItem] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <>
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
           <input
