c28c829db8d0db5ea1b63f56fe09deb184d79031
Mazhar <65758011+MostafaMazhar@users.noreply.github.com>
2023-06-18 04:47:53 +0000

    added deleteTodo function App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,24 @@ export default function App() {
         { id: crypto.randomUUID(), title: newItem, completed: false },
       ];
     });
+    setNewItem("");
+  }
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todos) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
   }
 
   return (
@@ -33,12 +51,21 @@ export default function App() {
       <ul className="list">
         {todos.map((todo) => {
           return (
-            <li>
+            <li key={todo.id}>
               <label>
-                <input type="checkbox" />
-                Item 1
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                />
+                {todo.title}
               </label>
-              <button className="btn btn-danger">Delete</button>
+              <button
+                onClick={() => deleteTodo(todo.id)}
+                className="btn btn-danger"
+              >
+                Delete
+              </button>
             </li>
           );
         })}
