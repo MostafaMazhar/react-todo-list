97dd664f3e3d7ade8374f425e7de863c32cce890
Mazhar <65758011+MostafaMazhar@users.noreply.github.com>
2023-06-18 04:17:32 +0000

    Update hardCode with functions -> App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,16 @@ import "./App.css";
 
 export default function App() {
   const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
   function handleSubmit(e) {
     e.preventDefault();
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ];
+    });
   }
 
   return (
@@ -23,20 +31,17 @@ export default function App() {
       </form>
       <h1 className="header">To Do List</h1>
       <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 2
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+        {todos.map((todo) => {
+          return (
+            <li>
+              <label>
+                <input type="checkbox" />
+                Item 1
+              </label>
+              <button className="btn btn-danger">Delete</button>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
